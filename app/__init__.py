6fef1486d7bd1aad53867b6e9965200bce668ccb
belensarome <138254555+belensarome@users.noreply.github.com>
2025-05-05 20:57:48 -0300

    configuración inicial básica de aplicación en flask

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_marshmallow import Marshmallow
+import os
+from app.config import config
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+migrate = Migrate()
+ma = Marshmallow()
+
+def create_app() -> None:
+
+    app_context = os.getenv('FLASK_CONTEXT')
+    app = Flask(__name__)
+
+    f = config.factory(app_context if app_context else 'development')
+    app.config.from_object(f)
+
+    ma.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    
+    @app.shell_context_processor    
+    def ctx():
+        return {"app": app}
+    
+    return app
